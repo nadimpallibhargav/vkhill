a27a7cc3de96a1271df5cc5a39cd0165c786f2c5
nadimpallibhargav <bhargavraju98@gmail.com>
2024-04-29 20:27:25 +0530

    💄sign up ui

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,69 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
+
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
 
 const SignUp = () => {
+  const[form, setForm] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const[isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center min-h-[85vh] px-4'>
+          <Text className='text-2xl text-white text-semibold font-psemibold pb-14'>Sign Up</Text>
+          <FormField 
+            title='Username'
+            value={form.username}
+            placeholder='Enter Username'
+            handleChangeText={(e) => setForm({...form,
+            username: e })}          
+          />
+          <FormField 
+            title='Email'
+            value={form.email}
+            keyboardType='email-address'
+            placeholder='Enter Email'
+            handleChangeText={(e) => setForm({...form,
+            email: e })}
+            otherStyles='pt-7'            
+          />
+          <FormField 
+            title='Password'
+            value={form.password}
+            placeholder='Enter Password'
+            handleChangeText={(e) => setForm({...form,
+            password: e })}
+            otherStyles='py-7' 
+          />
+
+          <CustomButton 
+            title='Sign Up'
+            handlePress={submit}
+            isLoading={isSubmitting}
+          />
+
+          <View className='flex-row justify-center items-center gap-2 pt-5'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Have an account already?
+            </Text>
+            <Link href='/sign-in' className='text-lg text-secondary font-psemibold'>Sign In</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
