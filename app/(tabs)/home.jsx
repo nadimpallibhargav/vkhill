e1c37c7eb64b66db4e2c1aec3f801f4960a4d35c
nadimpallibhargav <bhargavraju98@gmail.com>
2024-05-17 22:05:45 +0530

    💄added map

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar';
+
+import UserMap from '../../components/molecules/UserMap/UserMap';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full items-center justify-center min-h-[85vh] px-4">
+          <UserMap />
+        </View>        
+      </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light' />
+    </SafeAreaView>
   )
 }
 
