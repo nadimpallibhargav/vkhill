d9598bd46fda3643247a6313621fa0f8953e0269
nadimpallibhargav <bhargavraju98@gmail.com>
2024-05-28 16:42:57 +0530

    ♻️ updated signup submit

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { View, Text, ScrollView } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { Link, router } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 import FormField from '../../components/atoms/FormField/FormField'
 import CustomButton from '../../components/atoms/CustomButton/CustomButton'
